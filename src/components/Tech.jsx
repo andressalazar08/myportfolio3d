15fe8cf08282d2f0da2c087705eef233523b005b
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-06-15 17:19:24 -0500

    tech balls intro

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Ball from './canvas/Ball';
+import { SectionWrapper } from '../hoc';
+import { technologies } from '../constants';
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div
+      className="flex flex-row flex-wrap justify-center gap-10"
+    >
+      {technologies.map((techonology)=>(
+        <div
+          className="w-28 h-28"
+          key={techonology.name}
+        >
+
+          <Ball icon={techonology.icon}></Ball>
+
+        </div>
+      ))}
+    </div>
   )
 }
 
