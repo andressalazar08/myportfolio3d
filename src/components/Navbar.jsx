ceee1fed42d5a8656c221e03b6d217569b24cfd2
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-06-15 10:23:29 -0500

    navbar elmeents

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 
 import { styles } from '../styles'; //this is styles in the repo
 import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import { logo, menu, close, web } from '../assets';
+import aflogo from '../assets/andres-felipe-high-resolution-logo-white-on-transparent-background (1).png';
 //``
 const Navbar = () => {
 
@@ -22,10 +23,24 @@ const Navbar = () => {
               window.scrollTo(0,0);
             }}
             >
-              <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
+              <img src={aflogo} alt="logo"  style={{ width: 150, height: 250 }} className="w-1 h-1 object-contain"/>
 
-
+                {/* <p className="text-white text-[18px] font-bold cursor-pointer"> Andrés Felipe <span className="sm:block hidden">| Engineer</span> </p> */}
             </Link>
+            <p className="text-blue-500"> Bringing solutions through code</p>
+            <ul className="list-non hidden sm:flex flex-row gap-10">
+              {navLinks.map((link)=>(
+                <li key={link.id}
+                    className={`${
+                      active === link.title ? "text-white": "text-secondary"
+                    } hover: text-white text-[18px] font-medium cursor-pointer`}
+                >
+
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+
+            </ul>
         </div>
     </nav>
   )
