5d80d979fb23831c1b4abab9e29fce82306e7525
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-06-22 10:40:42 -0500

    about modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,10 @@ import { motion } from 'framer-motion';
 import  { styles }  from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import { github } from "../assets";
 
 import { SectionWrapper } from '../hoc';
+import { Link } from 'react-router-dom';
 //``
 const ServiceCard = ({ index, title, icon }) =>{
   return (
@@ -25,7 +27,7 @@ const ServiceCard = ({ index, title, icon }) =>{
                   speed: 450
                 }}
                 style={{background: "linear-gradient(#20e8e8, #207ee8)" }}
-                className="rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+                className="rounded-[20px] l:py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
               >
 
                 <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
@@ -41,6 +43,8 @@ const ServiceCard = ({ index, title, icon }) =>{
 const About = () => {
   return (
     <>
+      <div className="sm:flex flew-row ">
+      <div>
       <motion.div variants={textVariant()}>
 
           <h2 className={`${styles.sectionHeadText} mt-[110px]`}>
@@ -52,25 +56,42 @@ const About = () => {
         variants={fadeIn("","", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm Industrial Engineer and Systems Engineering student with experience in Javascript, Python and Java.
-        I had participated in different web app projects some of them as Product Owner and some as developer.
+        Bsc in Industrial Engineer, Msc in Engineer and  Systems Engineering student.<br />
+        I had participated in different web app projects, with different roles:
+        <br /><div className="text-[cyan] flex flex-row justify-between">Fullstack developer, Product Owner and Project Manager.</div>
+
         <br />
-        I also have been a trailblazer since 2023 ranked in the salesforce ecosystem.
+        <span className="text-[cyan] ">Trailblazer</span> ranked in the salesforce ecosystem as <span className="text-[cyan] ">Adventurer</span>.
         <br />I'm a quick learner and have strong skills on leadership and communication.
         <br />
-        Don't heasitate to contact me if you need a solution based on web technologies.
+        <br />
+        Contact me if you need a solution based on web technologies.
+        <br />
+        <br />
+        <div className="text-[cyan] text-xl">hi@andresfelipe.dev</div>
       </motion.p>
+    </div>
+      <motion.div variants={textVariant()}>
+        <div className="mt-[170px] ml-[150px]">
+            <Link
+              onClick={()=>{}}
+            >
+              <ServiceCard  key="certs" index="1" title="Certitications and Badges" icon={github}/>
+            </Link>
+        </div>
+      </motion.div>
 
+      </div>
         {/* cards  */}
 
-        <div className="mt-20 flex flex-wrap gap-10">
+        {/* <div className="mt-20 flex flex-wrap gap-10">
 
             {services.map((service, index)=>(
               <ServiceCard key={service.title} index={index} {...service}/>
             ))}
 
 
-        </div>
+        </div> */}
 
 
 
