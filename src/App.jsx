a2911f64df0d78a47bc7512bf493e9f19302481e
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-06-22 12:32:13 -0500

    projects cards hidden

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App=() =>{
           <About />
           <Experience />
 
-          <Works />
+          {/* <Works /> */}
           {/* <Feedbacks />  this is for testimonials*/}
 
           <div className="relative z-0">
